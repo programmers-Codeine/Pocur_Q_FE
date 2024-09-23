d81c1224335934463bc0f8b307963b740280cae5
ParkByunggwon <startartart@naver.com>
2024-09-23 11:30:13 +0900

    Refactor: options 상수 설정 및 map 구조분해할당 및 return 제거, 스타일 변수 제거

--- src/components/common/Options/ContextOptions.tsx
+++ src/components/common/Options/ContextOptions.tsx
@@ -1,34 +1,31 @@
+import { Fragment } from 'react/jsx-runtime';
 import { ContextOptionsProps } from './ContextOptions.types';
 
+const DEFAULT_OPTIONS = [
+  { id: 1, name: '적용하기' },
+  { id: 2, name: '수정하기' },
+  { id: 3, name: '삭제하기' },
+];
+
 export default function ContextOptions({
-  options = [
-    { id: 1, name: '적용하기' },
-    { id: 2, name: '수정하기' },
-    { id: 3, name: '삭제하기' },
-  ],
+  options = DEFAULT_OPTIONS,
   onClick,
 }: ContextOptionsProps) {
-  const firstOption = 'rounded-t-lg';
-  const bar = 'border-b border-d400 mx-2';
-  const lastOption = 'rounded-b-lg';
   return (
     <div className="bg-d10 m-2 border border-d400 rounded-lg">
-      {options.map((option, idx) => {
-        return (
-          <>
-            <div
-              className={`px-3 py-1 hover:bg-d30
-            ${idx === 0 && firstOption}
-            ${idx === options.length - 1 && lastOption}`}
-              key={option.id}
-              onClick={() => onClick(option.id)}
-            >
-              <p className="font-base text-d400">{option.name}</p>
-            </div>
-            {idx !== options.length - 1 && <div className={bar}></div>}
-          </>
-        );
-      })}
+      {options.map(({ id, name }, idx) => (
+        <Fragment key={id}>
+          <div
+            className={`px-3 py-1 hover:bg-d30
+            ${idx === 0 && 'rounded-t-lg'}
+            ${idx === options.length - 1 && 'rounded-b-lg'}`}
+            onClick={() => onClick(id)}
+          >
+            <p className="font-base text-d400">{name}</p>
+          </div>
+          {idx !== options.length - 1 && <div className="border-b border-d400 mx-2"></div>}
+        </Fragment>
+      ))}
     </div>
   );
 }
