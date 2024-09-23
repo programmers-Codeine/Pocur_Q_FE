3971631fef047c2d5f96e133df329cc1fbe82e4f
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2024-09-23 22:26:24 +0900

    [Feat] 인트로 화면 구현
    * Chore: pocurq logo phone 이미지 추가
    
    * Feat: AdminHome '/' 페이지 구성 및 라우팅

--- src/components/common/Layout/AdminHome.tsx
+++ src/components/common/Layout/AdminHome.tsx
@@ -1,21 +1,39 @@
+import { useNavigate } from 'react-router-dom';
+import logoPhone from '@/assets/images/pocurq_phone2.png';
+
 export default function AdminHome() {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex w-screen h-screen gap-1.5 p-2 bg-d10">
-      <aside className="flex items-center justify-center w-[40%] bg-b100">
-        <div>
-          <p>내 가게를</p>
-          <p>내 손으로</p>
-          <p>커스터마이징</p>
+    <div className="flex w-screen h-screen gap-1.5 p-2 bg-d10" onClick={() => navigate('/login')}>
+      <aside className="flex items-center justify-center w-[40%] bg-b100 relative">
+        <img src={logoPhone} className="absolute w-[40%] right-8 bottom-8" />
+        <div className="text-6xl text-d900 leading-tight z-10">
+          <p className="whitespace-pre-wrap">{`내 가게를\n내 손으로`}</p>
+          <p className="text-8xl font-bold">커스터마이징</p>
         </div>
       </aside>
       <main className="flex items-center justify-center w-[60%] bg-b50">
-        <div>
-          <p>나만의 멋진 메뉴판을 꾸미고</p>
-          <p>실시간으로 주문현황을 확인,</p>
-          <p>메뉴를 손쉽게 관리하세요 !</p>
-          <p>매출과 통계를 한눈에 파악하고,</p>
-          <p>고객 맞춤형 경험을 제공해보세요.</p>
-        </div>
+        <p className="whitespace-pre-wrap text-6xl text-d900 font-bold leading-tight">
+          {`나만의 멋진 메뉴판`}
+          <span className="text-5xl text-d200 font-normal">을 꾸미고</span>
+          {`\n실시간`}
+          <span className="text-5xl text-d200 font-normal">으로</span>
+          {` 주문현황을 확인`}
+          <span className="text-5xl text-d200 font-normal">,</span>
+          {`\n메뉴`}
+          <span className="text-5xl text-d200 font-normal">를 손쉽게</span>
+          {` 관리`}
+          <span className="text-5xl text-d200 font-normal">하세요 !</span>
+          {`\n\n매출과 통계`}
+          <span className="text-5xl text-d200 font-normal">를</span>
+          {` 한눈에 파악`}
+          <span className="text-5xl text-d200 font-normal">하고,</span>
+          {`\n고객 맞춤형 경험`}
+          <span className="text-5xl text-d200 font-normal">을</span>
+          {` 제공`}
+          <span className="text-5xl text-d200 font-normal">해보세요.</span>
+        </p>
       </main>
     </div>
   );
