14131fe01c738a38839c1848ff9023b5ee4a9022
nulzi-laptop <kimty131@naver.com>
2024-09-24 18:36:41 +0900

    Refactoring: svg 모듈화
    - 기존에 하나하나 import하는 방식에서 하나로 통일

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,6 @@
+import Avatar from '@/assets/icons/avatar.svg';
+import Logout from '@/assets/icons/logout.svg';
+import Setting from '@/assets/icons/setting.svg';
+import CircleArrow from '@/assets/icons/circleArrow.svg';
+
+export {Avatar, Logout, Setting, CircleArrow}
