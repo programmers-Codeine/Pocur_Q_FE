21d02bf83a8d668ecca6a90fb0238612898ac64b
ParkByunggwon <startartart@naver.com>
2024-09-24 11:06:39 +0900

    Feat: 라우팅 상수 정의

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,43 +5,57 @@ import { ErrorPage } from '@/pages/Error';
 import { LoginPage, RegisterPage } from '@/pages/AdminHome';
 import { SettingPage, MenuPage, DesignPage, EtcPage } from '@/pages/Setting';
 import { ManagePage, RealTimeMenuPage, StatisticsPage, TablePage } from '@/pages/Manage';
+import {
+  ROUTE_LOGIN,
+  ROUTE_REGISTER,
+  ROUTE_ADMIN,
+  ROUTE_MANAGE,
+  ROUTE_TABLE,
+  ROUTE_REAL_TIME_MENU,
+  ROUTE_STATISTICS,
+  ROUTE_SETTING,
+  ROUTE_MENU,
+  ROUTE_DESIGN,
+  ROUTE_ETC,
+  ROUTE_CUSTOMER,
+} from '@/constants/routing';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AdminHome />,
     children: [
-      { path: 'login', element: <LoginPage /> },
-      { path: 'register', element: <RegisterPage /> },
+      { path: ROUTE_LOGIN, element: <LoginPage /> },
+      { path: ROUTE_REGISTER, element: <RegisterPage /> },
     ],
     errorElement: <ErrorPage />,
   },
   {
-    path: '/admin',
+    path: `/${ROUTE_ADMIN}`,
     element: <Layout />,
     children: [
       {
-        path: 'manage',
+        path: ROUTE_MANAGE,
         element: <ManagePage />,
         children: [
-          { path: 'table', element: <TablePage /> },
-          { path: 'real-time-menu', element: <RealTimeMenuPage /> },
-          { path: 'statistics', element: <StatisticsPage /> },
+          { path: ROUTE_TABLE, element: <TablePage /> },
+          { path: ROUTE_REAL_TIME_MENU, element: <RealTimeMenuPage /> },
+          { path: ROUTE_STATISTICS, element: <StatisticsPage /> },
         ],
       },
       {
-        path: 'setting',
+        path: ROUTE_SETTING,
         element: <SettingPage />,
         children: [
-          { path: 'menu', element: <MenuPage /> },
-          { path: 'design', element: <DesignPage /> },
-          { path: 'etc', element: <EtcPage /> },
+          { path: ROUTE_MENU, element: <MenuPage /> },
+          { path: ROUTE_DESIGN, element: <DesignPage /> },
+          { path: ROUTE_ETC, element: <EtcPage /> },
         ],
       },
     ],
   },
   {
-    path: '/customer',
+    path: `/${ROUTE_CUSTOMER}`,
     element: <Layout />,
   },
 ]);
