d564e582e69d1941d8883dfde91d756b21149dc1
nulzi-laptop <kimty131@naver.com>
2024-09-25 14:21:43 +0900

    Refactor: 모달 버튼 텍스트를 prop이 아닌 children으로 수정
    - props를 types 파일로 분리

--- src/components/common/Modal/Button/ModalButton.tsx
+++ src/components/common/Modal/Button/ModalButton.tsx
@@ -1,21 +1,21 @@
 import clsx from 'clsx';
+import { PropsWithChildren } from 'react';
+import { ModalButtonProps } from './ModalButton.types';
 
-interface Props {
-  text: string;
-  onClick: () => void;
-  type?: 'warn' | 'default';
-}
-
-export default function ModalButton({ onClick, text, type = 'default' }: Props) {
+export default function ModalButton({
+  onClick,
+  children,
+  type = 'default',
+}: PropsWithChildren<ModalButtonProps>) {
   return (
     <button
       className={clsx(
-        'min-w-[170px] max-w-[200px] flex-1 text-base font-bold px-[12px] py-[8px] bg-d30 border border-d200 rounded-lg',
+        'min-w-[170px] max-w-[200px] flex-1 rounded-lg border border-d200 bg-d30 px-[12px] py-[8px] text-base font-bold',
         type !== 'default' ? 'text-highlightRed' : 'text-b500'
       )}
       onClick={onClick}
     >
-      {text}
+      {children}
     </button>
   );
 }
