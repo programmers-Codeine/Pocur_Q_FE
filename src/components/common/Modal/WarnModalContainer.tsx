edb1096da67978270f533f400c759fabde6ed038
nulzi-laptop <kimty131@naver.com>
2024-09-25 15:59:35 +0900

    Feat: 경고 모달 기능 추가
    - createPortal을 이용한 모달 호출 기능 구현
    - onClose 속성을 사용해 외부 클릭 시 모달 닫기 기능 구현
    - onClose, autoClose 속성을 사용해 자동 닫기 기능 구현

--- src/components/common/Modal/WarnModalContainer.tsx
+++ src/components/common/Modal/WarnModalContainer.tsx
@@ -1,14 +1,41 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
+import { WarnModalProps } from './Modal.types';
+import { Info } from '@/assets/icons';
+import { createPortal } from 'react-dom';
 
-interface Props extends PropsWithChildren {
-  iconUrl: string;
-}
+export default function WarnModalContainer({
+  open,
+  onClose,
+  autoClose = 0,
+  children,
+}: PropsWithChildren<WarnModalProps>) {
+  const modalElement = document.getElementById('modal');
 
-export default function WarnModalContainer({ children, iconUrl }: Props) {
-  return (
-    <div className="flex items-start gap-6 p-6 border border-d200 rounded-lg min-w-[440px] max-w-[550px]">
-      <img className="w-8 h-8" src={iconUrl} alt="modal icon image" />
-      <div className="flex flex-col flex-1">{children}</div>
-    </div>
+  useEffect(() => {
+    if (open && autoClose && onClose) {
+      const closeTimeout = setTimeout(() => onClose(), autoClose);
+
+      return () => clearTimeout(closeTimeout);
+    }
+  }, [open]);
+
+  if (!open || !modalElement) return null;
+
+  return createPortal(
+    <>
+      <div
+        className="fixed bottom-0 left-0 right-0 top-0 z-40 backdrop-blur-sm"
+        onClick={onClose}
+      />
+      <div
+        className="fixed left-1/2 top-1/2 z-50 flex min-w-[440px] -translate-x-1/2 -translate-y-1/2 items-start gap-6 rounded-lg border border-d200 bg-d10 p-6"
+        onClick={onClose}
+      >
+        {/* <img className="h-8 w-8" src={iconUrl} alt="modal icon image" /> */}
+        <Info />
+        <div className="flex flex-1 flex-col">{children}</div>
+      </div>
+    </>,
+    modalElement
   );
 }
