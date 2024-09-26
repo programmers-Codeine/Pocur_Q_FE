7a88c3f5c9f5032ea4d126ff5a4e8be62630c0b1
ParkByunggwon <startartart@naver.com>
2024-09-26 14:00:37 +0900

    Squashed commit of the following:
    commit 650aace0a9858006b121e6123b260166281946c2
    Author: ParkByunggwon <startartart@naver.com>
    Date:   Thu Sep 26 05:28:52 2024 +0900
    
        Feat: MenuPage 프레임 총 구성
    
    commit 23ca9487d865407c64b16927691b13d0aa725927
    Author: ParkByunggwon <startartart@naver.com>
    Date:   Thu Sep 26 05:27:13 2024 +0900
    
        Fix: Input 컴포넌트 props 수정
    
    commit 2001598c634b493e2aa636b551cc8dd7a796cff3
    Author: ParkByunggwon <startartart@naver.com>
    Date:   Thu Sep 26 05:26:25 2024 +0900
    
        Feat: ItemButton 컴포넌트, 스토리북 퍼블리싱
    
    commit db56fc5e18ddbcd6a6f0d0f9dd7b99200b10e029
    Author: ParkByunggwon <startartart@naver.com>
    Date:   Thu Sep 26 05:25:58 2024 +0900
    
        Chore: svg 추가 및  svg 타입추론 적용

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,25 @@
+import Plus from './plus.svg';
+import Search from './search.svg';
+import Exclamation from './exclamation.svg';
+import Save from './save.svg';
+import PencilQuestion from './pencil-question.svg';
+import NoImage from './no-image.svg';
+import Edit from './edit.svg';
+import Trash from './trash.svg';
+import ToggleOn from './toggle-on.svg';
+import ToggleOff from './toggle-off.svg';
+import Modify from './modify.svg';
+
+export {
+  Plus,
+  Search,
+  Exclamation,
+  Save,
+  PencilQuestion,
+  NoImage,
+  Edit,
+  Trash,
+  ToggleOn,
+  ToggleOff,
+  Modify,
+};
