fbc0d510d244282dcf3f3609e6a3afd6ea45f21b
nulzi <74344132+nulzi@users.noreply.github.com>
2024-09-26 17:10:36 +0900

    [Feat] 테이블 탭 구현 (#25)
    * Feat: 테이블 관리 화면 상단 탭 구현
    
    * Style: props type 분리
    
    * Feat: 테이블 관리 화면 테이블 구현
    
    * Feat: 테이블 관리 화면 테이블 탭 구현
    
    * Style: 필요없는 div 태그 삭제
    
    * Design: Table 컴포넌트 내부 텍스트 색상 변경

--- src/pages/Manage/TablePage.tsx
+++ src/pages/Manage/TablePage.tsx
@@ -1,3 +1,79 @@
+import Button from '@/components/common/Button/Button';
+import { useState } from 'react';
+import Tabs from './Table/Tabs';
+import Table from './Table/Table';
+import Tab from './Table/Tab';
+
+const tableList = [
+  {
+    tableNo: 1,
+    orderList: [
+      { menuName: '돼지수육', menuQuantity: 1 },
+      { menuName: '소주', menuQuantity: 2 },
+      { menuName: '탕수육', menuQuantity: 1 },
+    ],
+    totalPrice: 0,
+    newOrderNo: 1,
+  },
+  { tableNo: 2, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 3, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 4, orderList: [], totalPrice: 0, newOrderNo: 2 },
+  { tableNo: 5, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 6, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 7, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 8, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 9, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 10, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 71, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 81, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 91, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 101, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 21, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 231, orderList: [], totalPrice: 0, newOrderNo: 0 },
+  { tableNo: 121, orderList: [], totalPrice: 0, newOrderNo: 0 },
+];
+
 export default function TablePage() {
-  return <div>TablePage</div>;
+  const [currentTab, setCurrentTab] = useState('table');
+
+  const handleAddTable = () => {};
+  const handleTabChange = (id: string) => {
+    setCurrentTab(id);
+  };
+
+  return (
+    <div className="flex h-full flex-col">
+      <Tabs>
+        <Tab id="table" index={0} currentTab={currentTab} handleTabChange={handleTabChange}>
+          테이블
+        </Tab>
+        <Tab id="qr" index={1} currentTab={currentTab} handleTabChange={handleTabChange}>
+          QR
+        </Tab>
+      </Tabs>
+      {currentTab === 'table' ? (
+        <div className="flex h-full flex-wrap content-start gap-5 overflow-y-scroll p-10">
+          {tableList.map(table => (
+            <Table table={table} />
+          ))}
+          <div
+            className="max-w-1/6 relative flex max-h-[170px] min-h-[155px] min-w-[240px] cursor-pointer flex-col items-center justify-center rounded-lg border border-d900 p-4 text-2xl font-bold text-d200 hover:text-d900"
+            onClick={handleAddTable}
+          >
+            테이블 추가+
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className="flex h-full flex-wrap content-start gap-5 overflow-y-scroll p-10">
+            QR list
+          </div>
+          <div className="flex justify-center gap-2 py-[6px]">
+            <Button title="전체 QR 이미지 저장" type="others" />
+            <Button title="전체 QR 프린트" type="others" />
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
