2d9a46017110a8d15450ec22a97cf2e99c7e8fb5
nulzi-laptop <kimty131@naver.com>
2024-09-30 18:17:10 +0900

    Style: 코드 리뷰에 따른 수정
    - 부실한 주석 추가 작성
    - leftPosition 컴포넌트 밖으로 분리
    - leftPosition 객체에서 문자열로 변경

--- src/pages/Manage/Table/Tab.tsx
+++ src/pages/Manage/Table/Tab.tsx
@@ -4,23 +4,18 @@ import { TabProps } from './Tab.types';
 
 export default function Tab({
   id,
-  index,
+  leftPosition,
   currentTab,
   handleTabChange,
   children,
 }: PropsWithChildren<TabProps>) {
-  const leftPosition: { [key: number]: string } = {
-    0: 'left-[0px]',
-    1: 'left-[130px]',
-  }; // 고정된 160px에 대한 값
-
   return (
     <div
       key={id}
       className={clsx(
         'absolute top-0 flex h-[50px] min-w-[160px] items-center justify-center rounded-tr-[30px] border border-d900 text-2xl font-semibold',
-        leftPosition[index],
-        index === 0 && 'z-[1]',
+        leftPosition,
+        id === 'table' && 'z-[1]',
         id === currentTab ? 'bg-d900 text-d10' : 'cursor-pointer bg-d10 text-d900 hover:bg-b50'
       )}
       onClick={() => handleTabChange(id)}
