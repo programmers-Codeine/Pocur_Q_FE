f025b066d3b770d3f9f07676f318b05cb82a5203
nulzi-laptop <kimty131@naver.com>
2024-09-30 17:40:49 +0900

    Feat: 전체 QR 코드 이미지 다운로드 기능 구현
    - QR이 2열로 저장

--- src/utils/QR.ts
+++ src/utils/QR.ts
@@ -29,3 +29,50 @@ const downloadQR = (tableNo: number) => {
 
   img.src = url;
 };
+
+const downloadAllQR = () => {
+  // QR 전체 다운로드
+  const svgs = document.querySelectorAll('#qrDiv svg.qr');
+
+  if (svgs.length === 0) return;
+
+  const canvas = document.createElement('canvas');
+  const context = canvas.getContext('2d');
+
+  const svgWidth = 500;
+  const svgHeight = 500;
+  const spacing = 50;
+  const totalWidth = svgWidth * 2 + spacing;
+  const totalHeight = (svgHeight + spacing) * Math.ceil(svgs.length / 2);
+
+  canvas.width = totalWidth;
+  canvas.height = totalHeight;
+
+  svgs.forEach((svg, i) => {
+    const data = new XMLSerializer().serializeToString(svg);
+    const svgBlob = new Blob([data], { type: 'image/svg+xml;charset=utf-8' });
+    const url = URL.createObjectURL(svgBlob);
+    const img = new Image();
+
+    img.onload = () => {
+      context?.drawImage(
+        img,
+        (i % 2) * (svgWidth + spacing),
+        Math.floor(i / 2) * (svgHeight + spacing),
+        svgWidth,
+        svgHeight
+      );
+      URL.revokeObjectURL(url);
+
+      if (i === svgs.length - 1) {
+        const a = document.createElement('a');
+        a.download = 'allQR.png';
+        a.href = canvas.toDataURL();
+        a.click();
+        a.remove();
+      }
+    };
+
+    img.src = url;
+  });
+};
