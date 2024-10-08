068e5ba960f7de0300f663b56f3c724554c95a54
nulzi-laptop <kimty131@naver.com>
2024-10-08 16:01:12 +0900

    Feat: 첫 사용자 확인 loginFirst 전역 상태 추가

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -3,12 +3,17 @@ import { UserType } from '@/types';
 
 type userState = {
   userInfo: UserType;
+  loginFirst: boolean;
+
+  setLoginFirst: (isFirstLogin: boolean) => void;
 };
 
-const useUserStore = create<userState>(() => ({
+const useUserStore = create<userState>(set => ({
   userInfo: {
     id: null,
   },
+  loginFirst: true,
+  setLoginFirst: isFirstLogin => set(() => ({ loginFirst: isFirstLogin })),
 }));
 
 export default useUserStore;
