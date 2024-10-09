05849c125e8d7171c5881a6ac17a72c1c81b5627
nulzi <74344132+nulzi@users.noreply.github.com>
2024-10-09 15:12:29 +0900

    [Feat] login API 연동 및 기타 구현 (#44)
    * Chore: ssl 파일 gitignore 추가
    
    * Chore: vite https 설정
    
    * Feat: 첫 사용자 확인 loginFirst 전역 상태 추가
    
    * Feat: login API 연동
    
    * Feat: 첫 사용자 확인 로직 적용
    
    * Feat: 로그인 에러 발생 시 인풋 하단에 에러 텍스트 출력
    
    * Feat: 첫 사용자 확인 loginFirst 전역 상태 추가
    
    * Style: console.log 삭제

--- src/pages/AdminHome/LoginPage.tsx
+++ src/pages/AdminHome/LoginPage.tsx
@@ -4,19 +4,39 @@ import Button from '@/components/common/Button/Button';
 import Input from '@/components/common/Input/Input';
 import { loginInputList } from '@/pages/AdminHome/formFieldsData';
 import { UserLoginFormTypes } from './AdminHome.types';
+import { login } from '@/apis/user.api';
+import useUserStore from '@/stores/useUserStore';
 
 export default function LoginPage() {
   const [loginForm, setLoginForm] = useState<UserLoginFormTypes>({
     email: '',
     password: '',
   });
+  const [subText, setSubText] = useState({ text: '', warn: true });
   const navigate = useNavigate();
+  const { setLoginFirst } = useUserStore();
 
   const handleNavigate = (src: string) => {
     // TODO: 로그인 버튼을 통한 접속에 대한 처리가 필요함.
     // TODO: 성공적으로 로그인되었을 떄, 사용자가 첫 사용인지 체크.
     // TODO: navigate options에 처음 사용자인지(등록한 가게가 있는지)에 대한 결과 전달
     navigate(`/${src}`);
+  };
+  const handleLogin = () => {
+    //TODO response에 대한 타입 처리
+    login(loginForm)
+      .then((res: { message: string; isFirstLogin: boolean }) => {
+        if (res.isFirstLogin) {
+          setLoginFirst(true);
+          navigate('/admin/manage');
+        } else {
+          setLoginFirst(false);
+          navigate('/admin/manage/table');
+        }
+      })
+      .catch(() => {
+        setSubText(prev => ({ ...prev, text: '이메일/비밀번호를 확인해주세요.' }));
+      });
   };
 
   const handleInputChange = (id: string, value: string) => {
@@ -34,10 +54,11 @@ export default function LoginPage() {
             placeholder={placeholder}
             value={loginForm[id as keyof UserLoginFormTypes]}
             handleInputChange={e => handleInputChange(id, e.target.value)}
+            subText={subText}
           />
         </div>
       ))}
-      <Button title="로그인" type="others" onClick={() => handleNavigate('admin/manage')} />
+      <Button title="로그인" type="others" onClick={handleLogin} />
       <div className="flex cursor-pointer gap-4 text-d900">
         <span className="underline hover:font-bold">비밀번호를 잃어버리셨나요 ?</span>
         <span className="underline hover:font-bold" onClick={() => handleNavigate('register')}>
--- src/pages/Manage/Table/FirstPage.types.tsx
+++
@@ -1,3 +0,0 @@
-export interface FirstPageProps {
-  setIsFirstTime: React.Dispatch<React.SetStateAction<boolean>>;
-}
