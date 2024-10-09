407da46f52cbf3a39f1d5253191fcba5307974c2
nulzi <74344132+nulzi@users.noreply.github.com>
2024-10-09 15:13:38 +0900

    [Feat] 회원가입 API 연동 및 기타 기능 구현 (#48)
    * Chore: ssl 파일 gitignore 추가
    
    * Chore: vite https 설정
    
    * Feat: 회원가입 API 연동
    
    * Feat: 회원가입 시 입력값 규칙 확인(임시)
    
    - 임시로 정해둔 최소한의 규칙 확인

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -1,5 +1,10 @@
-import { UserLoginFormTypes } from '@/pages/AdminHome/AdminHome.types';
+import { RegisterUserFormTypes, UserLoginFormTypes } from '@/pages/AdminHome/AdminHome.types';
 import { axiosClient } from './axios';
+
+export const register = async (registerData: RegisterUserFormTypes) => {
+  const response = await axiosClient.post('/users/join', registerData);
+
+  return response.status;
 
 export const login = async (loginData: UserLoginFormTypes) => {
   const response = await axiosClient.post('/users/login', loginData);
