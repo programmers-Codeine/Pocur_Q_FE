fb7bf125a2c3e4d0efe9c26d8816e75a22e46105
nulzi-laptop <kimty131@naver.com>
2024-10-09 16:59:58 +0900

    Fix: } 추가

--- src/apis/user.api.ts
+++ src/apis/user.api.ts
@@ -5,6 +5,7 @@ export const register = async (registerData: RegisterUserFormTypes) => {
   const response = await axiosClient.post('/users/join', registerData);
 
   return response.status;
+};
 
 export const login = async (loginData: UserLoginFormTypes) => {
   const response = await axiosClient.post('/users/login', loginData);
