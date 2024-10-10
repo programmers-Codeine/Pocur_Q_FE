030484cf39b88f2647a713074cfeec66811f2eb1
nulzi-laptop <kimty131@naver.com>
2024-10-10 13:49:26 +0900

    Style: ResponseTable 타입 변경

--- src/apis/restaurantTables.api.ts
+++ src/apis/restaurantTables.api.ts
@@ -1,12 +1,14 @@
 import { axiosClient } from './axios';
 
-export type ResponseTable = {
+export type ResponseAllTable = {
   id: string;
   table_num: number;
+  created_at: string;
+  updated_at: string;
 };
 
 export const getAllTables = async () => {
-  const response = await axiosClient.get<ResponseTable[]>('/restaurantTables');
+  const response = await axiosClient.get<ResponseAllTable[]>('/restaurantTables');
 
   return response.data;
 };
