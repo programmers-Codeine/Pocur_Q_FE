12d8f3ab94b846c90c64f8e7ad82b087a7da39c8
ParkByunggwon <startartart@naver.com>
2024-10-10 17:33:43 +0900

    Design: CustomerLayout 스타일링

--- src/components/common/Layout/CustomerLayout.tsx
+++ src/components/common/Layout/CustomerLayout.tsx
@@ -2,8 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 export default function CustomerLayout() {
   return (
-    <div>
-      <Outlet />
+    <div className="flex h-screen w-full items-center justify-around bg-b50">
+      <div className="max-[1024px]:hidden lg:text-lg lg:font-bold lg:text-gray-700">
+        Customer Page in DeskTop
+      </div>
+
+      <div className="customer:w-[430px] h-full w-full border border-d10 bg-d10 shadow-lg">
+        <Outlet />
+      </div>
     </div>
   );
 }
