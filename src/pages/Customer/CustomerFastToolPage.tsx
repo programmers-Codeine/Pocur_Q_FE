e3299c43cc775c7342bf0fbe3566062a291f9ade
nulzi-laptop <kimty131@naver.com>
2024-10-14 18:07:13 +0900

    Feat: 빠른 호출 화면 구현

--- src/pages/Customer/CustomerFastToolPage.tsx
+++ src/pages/Customer/CustomerFastToolPage.tsx
@@ -1,3 +1,26 @@
+import { Bell } from '@/assets/icons';
+import IconButton from '@/components/customer/IconButton';
+import NavHeader from '@/components/customer/NavHeader';
+
+const fastToolList = ['직원 호출', '영수증', '자리 이동'];
+
 export default function CustomerFastToolPage() {
-  return <div>CustomerFastToolPage</div>;
+  return (
+    <div className="flex h-full flex-col overflow-y-scroll">
+      <NavHeader>빠른 호출</NavHeader>
+      {/* 주문 버튼 */}
+      {fastToolList.map(title => (
+        <div className="self-center py-2">
+          <IconButton
+            title={title}
+            onClick={() => {
+              // TODO 각 핸들러 구현
+            }}
+          >
+            <Bell />
+          </IconButton>
+        </div>
+      ))}
+    </div>
+  );
 }
