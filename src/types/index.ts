2b82a05bfb3cd2b1b4481f2127a3c0675d1946c3
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2024-10-15 14:43:24 +0900

    [Feat] 관리자 디자인 페이지 API 구현 (#67)
    * Feat: DesignPage API 구성 및 타입 수정
    
    * Fix: 임시 주석 사용

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
 export type UserType = {
-  id: string | null;
+  id: string | string | null;
 };
 
 export type UserFormTypes = {
@@ -28,8 +28,8 @@ export type InputMenuFormTypes = {
   menuCategory: string;
   price: string;
   origin: string;
+  options: AddOptionsTypes[] | string | null;
   image: string;
-  options: AddOptionsTypes[] | null;
   optionsInput: AddOptionsTypes[];
 };
 
@@ -81,6 +81,63 @@ export type InputEtcFormTypes = {
     prepayment: boolean;
   };
 };
+
+export type SetDesignPresetData = {
+  id: string;
+  name: string;
+};
+
+export type SetDesignData = {
+  id?: string;
+  name: string;
+  background: string | null;
+  bigText: string | null;
+  smallText: string | null;
+  box: string | null;
+  boxBorder: string | null;
+  icon: string | null;
+  buttonBackground: string | null;
+  buttonText: string | null;
+  buttonBorder: string | null;
+  buttonActiveBackground: string | null;
+  buttonActiveText: string | null;
+  buttonActiveBorder: string | null;
+  labelHot: string | null;
+  labelNew: string | null;
+  labelSoloOut: string | null;
+};
+
+// designName: '',
+//     designImage: '',
+//     theme: {
+//       all: {
+//         background: '#ffffff',
+//         largeText: '#ffffff',
+//         smallText: '#ffffff',
+//         box: '#ffffff',
+//         boxOutline: '#ffffff',
+//         icon: '#ffffff',
+//       },
+//       button: {
+//         normal: {
+//           background: '#ffffff',
+//           textAndIcon: '#ffffff',
+//           outline: '#ffffff',
+//         },
+//         active: {
+//           background: '#ffffff',
+//           textAndIcon: '#ffffff',
+//           outline: '#ffffff',
+//         },
+//       },
+//       addOption: {
+//         label: {
+//           hot: '#ffffff',
+//           new: '#ffffff',
+//           soldOut: '#ffffff',
+//         },
+//       },
+//     },
 
 export type SetMenuData = {
   id?: string;
