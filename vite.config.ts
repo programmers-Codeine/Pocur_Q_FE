78b15c1662aa2201d96db9117e19419f337c735b
ParkByunggwon <startartart@naver.com>
2024-10-15 14:54:39 +0900

    [Fix] vite production 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import fs from 'fs';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
 import 'dotenv/config';
 
-const fileName = fileURLToPath(import.meta.url);
-const dirName = path.dirname(fileName);
+// const fileName = fileURLToPath(import.meta.url);
+// const dirName = path.dirname(fileName);
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,12 +25,12 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(dirName, 'key.pem')),
-      cert: fs.readFileSync(path.resolve(dirName, 'cert.pem')),
-    },
-  },
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync(path.resolve(dirName, 'key.pem')),
+  //     cert: fs.readFileSync(path.resolve(dirName, 'cert.pem')),
+  //   },
+  // },
   define: {
     'process.env': process.env,
   },
