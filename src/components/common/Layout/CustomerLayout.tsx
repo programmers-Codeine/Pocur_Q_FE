a648b1d3b56c54d7a79f836533e9505948f2b49e
nulzi-laptop <kimty131@naver.com>
2024-10-16 10:16:31 +0900

    Feat: QR 접속 시 토큰 API 연동 및 Home 화면 이동

--- src/components/common/Layout/CustomerLayout.tsx
+++ src/components/common/Layout/CustomerLayout.tsx
@@ -1,6 +1,39 @@
-import { Outlet } from 'react-router-dom';
+import { getCustomerToken } from '@/apis/customer.api';
+import useCustomerStore from '@/stores/useCustomerStore';
+import { useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 export default function CustomerLayout() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { setCustomerTableNo } = useCustomerStore();
+
+  const handleMoveHome = async () => {
+    if (location.search !== '') {
+      try {
+        const [restaurantId, tableNum] = location.search
+          .replace('?restaurant_id=', '')
+          .replace('&table_num=', ',')
+          .split(',');
+        const jwtStatus = await getCustomerToken({
+          restaurantId,
+          tableNum,
+        });
+
+        if (jwtStatus === 200) {
+          setCustomerTableNo(Number(tableNum));
+          navigate('/customer/home');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleMoveHome();
+  }, []);
+
   return (
     <div className="flex h-screen w-full items-center justify-around bg-b50">
       <div className="max-[1024px]:hidden lg:text-lg lg:font-bold lg:text-gray-700">
