da1a7e33d13fdc01f1f8e47a01b7ab59744db3d6
Byunggwon Park <82306066+startartart@users.noreply.github.com>
2024-10-16 13:24:31 +0900

    [Feat] 관리자 기타 가게 관리 API 구현 (#72)
    * Feat: 기타 가게 관리 핸들러 및 API 연결
    
    * Feat: 소비자측 미리보기 추가

--- src/apis/restaurants.api.ts
+++ src/apis/restaurants.api.ts
@@ -1,4 +1,5 @@
 import { RestaurantDefaultData } from '@/pages/Manage/Table/FirstPage.types';
+import { SetEtcData } from '@/types';
 import { axiosClient } from './axios';
 import type { Restaurant } from '@/stores/useRestaurantStore';
 
@@ -10,6 +11,25 @@ export const createRestaurant = async (defaultData: RestaurantDefaultData) => {
 
 export const getRestaurant = async () => {
   const response = await axiosClient.get<Restaurant>('/restaurants');
+
+  return response.data;
+};
+
+export const saveRestaurants = async (restaurantsInfo: SetEtcData) => {
+  const response = await axiosClient.put('/restaurants', restaurantsInfo);
+
+  return response.data;
+};
+
+export const setLogoImage = async (file: File) => {
+  const formData = new FormData();
+  formData.append('upload', file);
+
+  const response = await axiosClient.post('/img-upload', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
 
   return response.data;
 };
