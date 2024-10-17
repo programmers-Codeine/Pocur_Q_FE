e6e060aa32d3cbb1945ff448dc1d955c249716ad
nulzi-laptop <kimty131@naver.com>
2024-10-17 15:56:10 +0900

    Feat: 빠른 도구 소켓 emit 메시지 변경

--- src/pages/Customer/CustomerFastToolPage.tsx
+++ src/pages/Customer/CustomerFastToolPage.tsx
@@ -1,12 +1,13 @@
 import { Bell } from '@/assets/icons';
 import IconButton from '@/components/customer/IconButton';
 import NavHeader from '@/components/customer/NavHeader';
-import useSocketStore from '@/stores/useCustomerSocketStore';
-
+import useCustomerSocketStore from '@/stores/useCustomerSocketStore';
+import useCustomerStore from '@/stores/useCustomerStore';
 const fastToolList = ['직원 호출', '영수증', '자리 이동'];
 
 export default function CustomerFastToolPage() {
-  const { socket } = useSocketStore();
+  const { socket } = useCustomerSocketStore();
+  const { customerTableNo } = useCustomerStore();
 
   return (
     <div className="flex h-full flex-col overflow-y-scroll">
@@ -20,8 +21,8 @@ export default function CustomerFastToolPage() {
               // TODO 각 핸들러 구현
               if (socket.connected) {
                 socket.emit('placeCallRequest', {
-                  callName: '수저 세트',
-                  tableNum: 1,
+                  callName: title,
+                  tableNum: customerTableNo,
                 });
               } else {
                 // TODO 소켓 미연결 시 소비자에게 QR 재접속 요청
